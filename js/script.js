b3e454263d2840bc20a1c6d9155cc04937f76498
Joe SIlver worknotebook <salup@gmail.com>
2022-02-08 18:32:43 +0500

    конец 27 урока по петруче

--- js/script.js
+++ js/script.js
@@ -1,22 +1,76 @@
 'use strict';
+/* 
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+console.log(typeof(5 + ''));
+console.log(typeof(5 + 2));
 
-function hello() {
-    console.log('Hello Joe Gazel Bee');
+const num = 5;
+console.log('http://vk.com/catalog/' + num);
+const fontSize = 26 + 'px';
+
+console.log(typeof(Number('4')));
+console.log(typeof(+'4'));
+console.log(typeof('zalupa'));
+console.log(typeof(+'zalupa'));
+console.log(+'zalupa');
+console.log(typeof(parseInt('15px', 10)));
+console.log(parseInt('15px', 10));
+
+let answ = +prompt('HEllo T4ert!', '');
+console.log(answ);
+
+
+let switcher = null
+if (switcher) {
+    console.log('Fuck you!')
+}
+    switcher = 1
+
+if (switcher) {
+    console.log('Fuck you!')
 }
 
-hello();
+console.log(typeof(Boolean('4')));
+console.log(typeof(!!'4444'));
+ */
 
-function hi() {
-    console.log('Say Hoy!');
-}
+/* let x = 5
+alert(++x)
+alert(x++) */
 
-hi();
+/* let y = 1
+let x = y = 2
+alert(x) */
 
-const arr = [1, 14, 4, 30, 54],
-    sorted = arr.sort(compareNum);
+/* console.log([] + false);
 
-function compareNum(a, b) {
-    return a - b;
-}
+console.log(typeof([] + false));
 
-console.log(sorted);
+console.log([] + false - null);
+console.log(typeof([] + false - null));
+
+console.log([] + false - null + true);
+console.log(typeof([] + false - null + true)); */
+
+/* console.log([] + 1 + 2); */
+
+/* alert('1'[0]) 
+alert('12345'[6])*/
+
+/* console.log(typeof([0])); */
+
+/* console.log(2 && 1 && null && 0 && undefined); // && запинается на лжи и выводит первое встреченое выражение false */
+
+/* console.log(!!(1 && 2) === (1 && 2)); */
+/* alert(null || 2 && 3 || 4) */
+
+/* const a = [1, 2, 3]
+const b = [1, 2, 3]
+console.log(a == b); */
+
+/* alert(+'Infinity')
+alert(+'infinity') */
+
+/* console.log('Ёжик' > 'ябпоко'); */
+console.log(0 || '' || 2 || undefined || true || false);
