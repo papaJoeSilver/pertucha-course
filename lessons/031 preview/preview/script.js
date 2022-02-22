028d1caa2d1df930bcf7b3c46b1a959699ecce6f
Chewyts <example@gmail.com>
2022-02-22 11:12:23 +0500

    Lesson 31 full

--- lessons/031 preview/preview/script.js
+++ lessons/031 preview/preview/script.js
@@ -1,5 +1,5 @@
-const btn = document.querySelector('button')
-
+const btns = document.querySelectorAll('button')
+      overlay = document.querySelector('.overlay')
 // btn.onclick = () => {
 //     alert('Под стук, под такт, это Click,  это Clack')
 // }
@@ -15,6 +15,39 @@ const btn = document.querySelector('button')
 //     alert('Под стук, под такт, это SECOND Click,  это SECOND Clack')
 // })
 
-btn.addEventListener('mouseenter', () => {
-    console.log('Hover');
+// btn.addEventListener('mouseenter', (event) => {
+//     console.log('Hover');
+//     // console.log(event.target);
+//     event.target.remove()
+// })
+
+// btn.addEventListener('click', (event) => {
+//     event.target.remove()
+// })
+
+//let i = 0;
+const deleteElement = (e) => {
+   // e.target.remove();
+   // console.log(e.target);
+   console.log(e.currentTarget);
+   console.log(e.type);
+//    i++;
+//    if (i == 3) {
+//     btn.removeEventListener('click', deleteElement);
+//    }
+}
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once:true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
 })
