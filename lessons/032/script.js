653c70f5dad9a5b58ea33caf0a45949658454190
Chewyts <example@gmail.com>
2022-02-22 14:03:01 +0500

    Lesson 32

---
+++ lessons/032/script.js
@@ -0,0 +1,25 @@
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling.previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling.previousElementSibling);
+
+// console.log(document.body.childNodes);
+
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue
+//     }
+
+//     console.log(node);
+// }
