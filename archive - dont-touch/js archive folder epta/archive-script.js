bde6065f864200e1e586304d81df95bd00f436ae
Chewyts <example@gmail.com>
2022-03-21 21:26:41 +0500

    2022.03.21

--- archive - dont-touch/js archive folder epta/archive-script.js
+++ archive - dont-touch/js archive folder epta/archive-script.js
@@ -706,3 +706,16 @@ console.log(oneHeart);
 const twoHeart = document.querySelector('div')
 console.log(twoHeart); */
 
+/* const p = document.querySelectorAll('p');
+console.log(p);
+
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript("js/test.js");
+loadScript("js/some.js"); */
