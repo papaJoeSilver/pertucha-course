2ed43d17a6b9da5166aea65f4dac2e258572b372
Chewyts <example@gmail.com>
2022-03-22 19:27:25 +0500

    2022.03.22-01

--- lessons/037/js/script.js
+++ lessons/037/js/script.js
@@ -4,11 +4,18 @@ const btns = document.querySelectorAll('button');
 
 /* console.log(btns[0].classList.length); */
 /* console.log(btns[0].classList.item(1)); */
-console.log(btns[1].classList.add('red', 'HOY'));
+/* console.log(btns[1].classList.add('red', 'HOY')); */
 /* console.log(btns[0].classList.remove('blue')); */
 // console.log(btns[0].classList.toggle('blue'));
 
-if (btns[1].classList.contains('red')) {
+/* if (btns[1].classList.contains('red')) {
     console.log('red');
-}
-xzdfas
+} */
+
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+});
